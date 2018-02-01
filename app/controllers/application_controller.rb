cbd8171fdc3a2eae0125ae1e34846ea4c5eac817
DaniG2k <byakugan.87@gmail.com>
2018-02-01 23:46:23 +0100

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
 
-  # before_filter :authenticate  
-  
+  # before_filter :authenticate
+
   # def current_user
   #     @current_user ||= super && User.includes(:organization).find(@current_user.id)
   # end
-  # 
+  #
 
   helper_method :admin?
 
@@ -17,22 +18,30 @@ class ApplicationController < ActionController::Base
     end
     return false
   end
-  
+
   def protect_admin
     unless admin?
       redirect_to root_path
     end
   end
 
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
+
   protected
-    def authenticate_inviter!
-      protect_admin
-    end
+
+  def authenticate_inviter!
+    protect_admin
+  end
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
       username == "ciancio" && password == "forpresident"
     end
   end
-  
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,13 @@
 #encoding: utf-8
 class HomeController < ApplicationController
-  
   def index
     @events = Event.upcoming.order("date_begun").limit(4)
     @posts = Post.order("date_published DESC").limit(2)
-    
+
     # @projects = Project.order("RANDOM()").limit(6)
     @highlights = Highlight.order("weight")
-    
-    
+
+
     # set_meta_tags title: 'Homepage',
     #               description: 'Non Riservato è la piattaforma online per la riconquista creativa degli spazi pubblici di Milano. Coltiva, corri, scopri, gioca, assaggia: fai da te una città a misura di tutti!',
     #               og: {
@@ -20,11 +19,5 @@ class HomeController < ApplicationController
     #               fb:{
     #                 app_id:          '673640696130600'
     #               }
-                   
   end
-
 end
-
-
-
-
