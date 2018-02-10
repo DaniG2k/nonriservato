b416ce9a82c25bb434d4500c0fa62876eb1f988e
DaniG2k <byakugan.87@gmail.com>
2018-02-10 01:16:59 +0100

    Refactor pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,13 @@
 class PagesController < ApplicationController
+  before_filter :set_page, only: [:edit, :update]
   before_filter :protect_admin, only: [:edit, :update]
 
   def show
     @page = Page.find_by_slug(params[:slug])
     if @page.nil?
       if admin?
-        @page = Page.new(:slug => params[:slug])
-        render :action => :new
+        @page = Page.new(slug: params[:slug])
+        render action: 'new'
       else
         redirect_to '/404'
       end
@@ -16,24 +17,27 @@ class PagesController < ApplicationController
   def create
     @page = Page.new(params[:page])
     if @page.save
-      redirect_to "/pages/#{@page.slug}"
+      redirect_to page_path(@page.slug)
     else
       render action: 'edit'
     end
   end
 
-  def edit
-    @page = Page.find_by_id(params[:id])
-  end
+  def edit; end
 
   def update
-    @page = Page.find(params[:id])
     @page.update_attributes(params[:page])
 
     if @page.save
-      redirect_to "/pages/#{@page.slug}", notice: 'page was successfully created.'
+      redirect_to page_path(@page.slug), notice: 'page was successfully created.'
     else
       render action: 'edit'
     end
+  end
+
+  private
+
+  def set_page
+    @page = Page.find(params[:id])
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ActiveRecord::Base
   attr_accessible :text, :title, :slug
 
-  validates :title, :text, :presence => true
+  validates :title, :text, presence: true
 end
